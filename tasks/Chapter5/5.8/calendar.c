11a149facf5de46b9ca7663fcfb466c2269cbc01
Andrei Matys <matys.andrey@gmail.com>
2026-06-27 21:20:45 +0300

    added 5.8

---
+++ tasks/Chapter5/5.8/calendar.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+static char daytab[2][13] = {
+    { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 },
+    { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }
+};
+
+int day_of_year(int year, int month, int day)
+{
+    int i, leap;
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    for (i = 1; i < month; i++)
+        day += daytab[leap][i];
+    return day;
+}
+
+void month_day(int year, int yearday, int *pmonth, int *pday)
+{
+    int i, leap;
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    for (i = 1; yearday > daytab[leap][i]; i++)
+        yearday -= daytab[leap][i];
+    *pmonth = i;
+    *pday = yearday;
+}
+
+int main()
+{
+    int year, month, yearday, day;
+    year = 2026;
+    yearday = 33;
+    month = 0;
+    day = 0;
+    month_day(year, yearday, &month, &day);
+    printf("Year: %d, Yearday: %d. Calculdated month: %d day: %d\n", year, yearday, month, day);
+    return 0;
+}
